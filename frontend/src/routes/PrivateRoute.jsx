5f43cd6d6ea6b4805be5fb7287197f739d3884f8
miguel-cybersecdev <miguel.snts14.cybersecdev@gmail.com>
2026-04-01 09:27:47 -0300

    Subi uns arquivos do node que não sei e vou congelar esse projeto pra inicar outro

---
+++ frontend/src/routes/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+export default function PrivateRoute({ children }){
+    const { token } = useAuth();
+
+    return token ? children : <Navigate to="/" />;
+}
